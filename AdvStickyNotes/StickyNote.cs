247b4ebbb8fce3743560c236047d235332498615
zoomoc <zoomoc101@gmail.com>
2019-12-26 22:17:23 +0900

    개선

--- AdvStickyNotes/StickyNote.cs
+++ AdvStickyNotes/StickyNote.cs
@@ -11,9 +11,8 @@ namespace AdvStickyNotes
 {
     public partial class StickyNote : Form
     {
-        TextBox textBox1;
+        RichTextBox textBox1;
         Button closeBtn;
-
 
         public StickyNote()
         {
@@ -23,15 +22,20 @@ namespace AdvStickyNotes
 
         private void StickyNote_Load(object sender, EventArgs e)
         {
-            textBox1 = new TextBox();
-            textBox1.Width = 200;
-            textBox1.Height = 100;
+            textBox1 = new RichTextBox();
+            textBox1.Location = new Point(0, 20);
+            textBox1.Width = Size.Width;
+            textBox1.Height = Size.Height;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.BackColor = Color.FromArgb(248, 247, 182);
 
             closeBtn = new Button();
-            closeBtn.Location = new Point(200, 0);
+            closeBtn.Location = new Point(Size.Width-20, 0);
             closeBtn.Size = new Size(20, 20);
             closeBtn.Text = "X";
             closeBtn.Click += CloseBtn_Click;
+            closeBtn.FlatStyle = FlatStyle.Flat;
+            closeBtn.FlatAppearance.BorderSize = 0;
 
             this.Controls.Add(textBox1);
             this.Controls.Add(closeBtn);
@@ -39,9 +43,7 @@ namespace AdvStickyNotes
 
         private void CloseBtn_Click(object sender, EventArgs e)
         {
-            Form senderForm = sender as Form;
-            senderForm.Close();
-            throw new NotImplementedException();
+            Close();
         }
         
     }
